722b53269763e5e8f8730b4a648eb618cc763fa4
Ryan Weispfenning <weisp014@gmail.com>
2023-02-24 15:28:03 -0600

    created table for info in DOM and created handler for submit button

--- salary.js
+++ salary.js
@@ -1,1 +1,13 @@
-console.log('its working');
+let allEmployees = [];  //global variable to hold entire array of employee info
+
+$(document).ready(readyNow);
+
+function readyNow(){
+    //console.log("DOM loaded");
+    //add listener for submit button
+    $('#submitEmployee').on('click', addEmployee);
+}
+
+function addEmployee() {
+    console.log('in addEmployee()');
+}
